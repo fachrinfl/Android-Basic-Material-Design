8b3aa763b589d9a96da2995f265023da2e417f8a
Fachri naufal <fachrinfl>
2018-02-17 20:46:25 +0700

    add material navigation drawer

--- app/src/main/java/com/fachrinfl/android_basic_material_design/MainActivity.java
+++ app/src/main/java/com/fachrinfl/android_basic_material_design/MainActivity.java
@@ -1,13 +1,91 @@
 package com.fachrinfl.android_basic_material_design;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.navigation_drawer);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Menu");
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer);
+        drawerLayout.setDrawerListener(toggle);
+
+        toggle.syncState();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+
+            case R.id.inbox_id:
+                Toast.makeText(getApplicationContext(), R.string.str_menu_inbox, Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.starred_id:
+                Toast.makeText(getApplicationContext(), R.string.str_menu_starred, Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.sent_mail_id:
+                Toast.makeText(getApplicationContext(), R.string.str_menu_sent_mail, Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.drafts_id:
+                Toast.makeText(getApplicationContext(), R.string.str_menu_drafts, Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.all_mail_id:
+                Toast.makeText(getApplicationContext(), R.string.str_menu_all_mail, Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.trash_id:
+                Toast.makeText(getApplicationContext(), R.string.str_menu_trash, Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.spam_id:
+                Toast.makeText(getApplicationContext(), R.string.str_menu_spam, Toast.LENGTH_LONG).show();
+                break;
+
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
