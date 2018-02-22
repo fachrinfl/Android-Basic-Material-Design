c21534ade515b92cb07ca694f49f7578e4011104
Fachri naufal <fachrinfl>
2018-02-22 23:06:12 +0700

    add material bottom navigation

--- app/src/main/java/com/fachrinfl/android_basic_material_design/MainActivity.java
+++ app/src/main/java/com/fachrinfl/android_basic_material_design/MainActivity.java
@@ -1,13 +1,49 @@
 package com.fachrinfl.android_basic_material_design;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    BottomNavigationView bottomNavigationView;
+    TextView textView_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation_id);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        textView_id = (TextView) findViewById(R.id.textView_id);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+
+            case R.id.recents_id:
+                textView_id.setText("Recents");
+                return true;
+
+            case R.id.favorites_id:
+                textView_id.setText("Favorite");
+                return true;
+
+            case R.id.nearby_id:
+                textView_id.setText("Near by");
+                return true;
+        }
+
+        return false;
     }
 }
