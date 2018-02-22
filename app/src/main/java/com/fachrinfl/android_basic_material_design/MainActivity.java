6ecc5cba04600a70d0ea2db5882085f039a4a1b0
Fachri naufal <fachrinfl>
2018-02-22 23:52:57 +0700

    add material swipe refresh

--- app/src/main/java/com/fachrinfl/android_basic_material_design/MainActivity.java
+++ app/src/main/java/com/fachrinfl/android_basic_material_design/MainActivity.java
@@ -1,13 +1,55 @@
 package com.fachrinfl.android_basic_material_design;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout swipeRefreshLayout;
+    WebView webView;
+    WebViewClient webViewClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefresh_id);
+        webView = (WebView) findViewById(R.id.webView_id);
+        webView.loadUrl("https://www.google.com");
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();
+            }
+        });
+
+        webViewClient = new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onPageCommitVisible(WebView view, String url) {
+                super.onPageCommitVisible(view, url);
+                swipeRefreshLayout.setColorSchemeColors(Color.parseColor("#4CAF50"));
+            }
+
+            @Override
+            public void onLoadResource(WebView view, String url) {
+                super.onLoadResource(view, url);
+                swipeRefreshLayout.setColorSchemeColors(Color.parseColor("#FF5722"));
+            }
+        };
+
+        webView.setWebViewClient(webViewClient);
+
     }
 }
